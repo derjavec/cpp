cea0f87192db6f22a68ce78388d623da2549e582
Denise Erjavec <deniseerjavec@Air-de-Denise.lan>
2024-11-01 10:44:58 +0100

    cpp03 ex01

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deniseerjavec <deniseerjavec@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 09:46:16 by derjavec          #+#    #+#             */
+/*   Updated: 2024/10/31 22:22:02 by deniseerjav      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+#include <iostream>
+
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+
+class ClapTrap
+{
+private:
+    std::string name;
+    int         HitPoints;
+    int         EnergyPoints;
+    int         AttackDamage;
+
+public:
+    ClapTrap();
+    ClapTrap(std::string name);
+    ~ClapTrap();
+    ClapTrap(const ClapTrap& obj);
+    ClapTrap& operator=(const ClapTrap& other);
+    
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    int  getHitPoints();
+    int  getEnergyPoints();
+    int  getAttackDamage();
+    void setAttackDamage(int amount);
+};
+
+#endif
