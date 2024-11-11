c6db6b5a0d06dc10393d2bf381ab3c2a93a2e9bc
Denise Erjavec <derjavec@k2r3p1.42mulhouse.fr>
2024-11-11 16:21:24 +0100

    cpp006

---
+++ cpp06/ex00/ScalarConvert.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConvert.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: derjavec <derjavec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 13:37:38 by derjavec          #+#    #+#             */
+/*   Updated: 2024/11/11 16:14:24 by derjavec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERT_HPP
+#define SCALARCONVERT_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+class ScalarConvert
+{ 
+     public:
+          static void convert(const std::string str);
+};
+#endif
